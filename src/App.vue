882120232032b6c236ead46420ec9fec28a073b9
ousmane <ousmanely11@gmail.com>
2024-08-08 14:24:15 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <EmployeeList />
+  </div>
+</template>
+
+<script>
+import EmployeeList from './components/EmployeeList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    EmployeeList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/EmployeeList.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="employee-list">
+      <ol>
+        <li v-for="employee in employees" :key="employee.id" class="employee-item">
+          {{ employee.name }}
+        </li>
+      </ol>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'EmployeeList',
+    data() {
+      return {
+        employees: [
+          { id: 1, name: 'Ly Ousmane' },
+          { id: 2, name: 'Traoré Harouna' },
+          { id: 3, name: 'Seme Thiello' },
+          { id: 4, name: 'Ba Zakaria' }
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .employee-list {
+    max-width: 500px;
+    margin: auto;
+    padding: 30px;
+    font-family: Arial, sans-serif;
+  }
+  
+  .employee-list ol {
+    align-items: center;
+    list-style-type: decimal;
+    padding-left: 20px;
+    cursor: pointer;
+  }
+  
+  .employee-item {
+    color: rgb(255, 255, 255);
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    margin-bottom: 10px;
+    transition: background-color 0.3s ease;
+  }
+  
+  .employee-item:hover {
+    background-color: #35c53ab6;
+  }
+  </style>
+  
